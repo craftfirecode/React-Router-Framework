ccf50142e08cc8de465037f1088899053688246b
Jokoson <exampleexampleexample12135example@example.com>
2025-02-23 10:42:35 +0100

    Init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,23 @@
+import type { Route } from "../+types/root";
+import {Button} from "@/components/ui/button";
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+}
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to React Router!</h1>
+            <p>
+                This is a new React Router app. It uses the latest version of
+                React Router and is pre-configured with server-side rendering
+                and other best practices.
+            </p>
+            <Button>Click me</Button>
+        </div>
+    );
+}
