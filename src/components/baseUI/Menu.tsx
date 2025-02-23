3888349dfae853e77f3224cd558db1b0340de63d
Jokoson <exampleexampleexample12135example@example.com>
2025-02-23 19:14:53 +0100

    Add navigation links to MegaMenu

--- src/components/baseUI/Menu.tsx
+++ src/components/baseUI/Menu.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import {Menu} from '@base-ui-components/react/menu';
+import {Link} from "react-router";
 
 export default function MegaMenu() {
     return (
         <nav className="bg-gray-100 py-2">
             <div className="container mx-auto flex gap-4 items-center">
                 <div>BRAND</div>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/about"}>About</Link>
                 <Menu.Root openOnHover>
                     <Menu.Trigger
                         className="flex h-10 items-center justify-center gap-1.5 rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:bg-gray-100">
-                        Add to playlist <ChevronDownIcon className="-mr-1"/>
+                        Blog<ChevronDownIcon className="-mr-1"/>
                     </Menu.Trigger>
                     <Menu.Portal>
                         <Menu.Positioner className="outline-none" sideOffset={8}>
@@ -21,55 +24,15 @@ export default function MegaMenu() {
                                 </Menu.Arrow>
                                 <Menu.Item
                                     className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Get Up!
+                                    <Link to={"/blog/5"}>Blog 5</Link>
                                 </Menu.Item>
                                 <Menu.Item
                                     className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Inside Out
+                                    <Link to={"/blog/10"}>Blog 10</Link>
                                 </Menu.Item>
                                 <Menu.Item
                                     className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Night Beats
-                                </Menu.Item>
-                                <Menu.Separator className="mx-4 my-1.5 h-px bg-gray-200"/>
-                                <Menu.Item
-                                    className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    New playlist…
-                                </Menu.Item>
-                            </Menu.Popup>
-                        </Menu.Positioner>
-                    </Menu.Portal>
-                </Menu.Root>
-
-                <Menu.Root openOnHover>
-                    <Menu.Trigger
-                        className="flex h-10 items-center justify-center gap-1.5 rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100 data-[popup-open]:bg-gray-100">
-                        Add to playlist <ChevronDownIcon className="-mr-1"/>
-                    </Menu.Trigger>
-                    <Menu.Portal>
-                        <Menu.Positioner className="outline-none" sideOffset={8}>
-                            <Menu.Popup
-                                className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
-                                <Menu.Arrow
-                                    className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
-                                    <ArrowSvg/>
-                                </Menu.Arrow>
-                                <Menu.Item
-                                    className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Get Up!
-                                </Menu.Item>
-                                <Menu.Item
-                                    className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Inside Out
-                                </Menu.Item>
-                                <Menu.Item
-                                    className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    Night Beats
-                                </Menu.Item>
-                                <Menu.Separator className="mx-4 my-1.5 h-px bg-gray-200"/>
-                                <Menu.Item
-                                    className="flex cursor-default py-2 pr-8 pl-4 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900">
-                                    New playlist…
+                                    <Link to={"/blog/15"}>Blog 15</Link>
                                 </Menu.Item>
                             </Menu.Popup>
                         </Menu.Positioner>
