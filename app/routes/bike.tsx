9aa709bdcc6f7d2e77d3de11d00e5cf9ce9c5f84
Jokoson <exampleexampleexample12135example@example.com>
2025-03-01 14:35:31 +0100

    Add Accordion component and integrate into Bike page for enhanced UI interaction

--- app/routes/bike.tsx
+++ app/routes/bike.tsx
@@ -2,6 +2,7 @@ import type {Route} from "./+types/bike";
 import axios from "axios";
 import LayoutContainer from "@/layout/layoutContainer";
 import {useState} from "react";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
 
 export async function loader({params}: Route.LoaderArgs) {
     let response = await axios.get(`https://jsonplaceholder.typicode.com/posts/1`);
@@ -24,6 +25,30 @@ export default function Product({loaderData}: Route.ComponentProps) {
             {title}<br/>
             {csr}<br/>
             {name}
+            <div>
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Is it styled?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Is it animated?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It&apos;s animated by default, but you can disable it if you
+                            prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
         </LayoutContainer>
     );
 }
