65164e97f707c92ab5728e057ac7b418c9989f78
Jokoson <exampleexampleexample12135example@example.com>
2025-03-02 13:52:08 +0100

    Rename ProtectedLayout to ProtectedAccountInvoiceIndex and enhance user redirection handling

--- app/routes/_protected.tsx
+++ app/routes/_protected.tsx
@@ -2,14 +2,19 @@ import {useContext, useEffect} from "react";
 import {Outlet} from "react-router";
 import {SupabaseAuthContext} from "~/lib/supabaseAuthProvider";
 
-export default function ProtectedLayout() {
+export default function ProtectedAccountInvoiceIndex() {
     const {user, session, isAuthenticating} = useContext(SupabaseAuthContext);
+
     useEffect(() => {
         if (!user && !isAuthenticating) {
             localStorage.setItem("from", location.href);
             location.replace("/login");
         }
     }, [user, session, isAuthenticating]);
+
+    if (!user) {
+        return null; // Render nothing while redirecting
+    }
 
     return <Outlet/>;
 }
