8f8af8299de9b3cc48d703531a72f594994c7c2a
Jokoson <exampleexampleexample12135example@example.com>
2025-03-03 11:32:28 +0100

    Replace tabs with CardFx components in blog index for improved layout

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -3,11 +3,9 @@ import {Outlet} from "react-router";
 export default function ProjectLayout() {
     return (
         <div>
-            <div>Example Layout</div>
-            <main>
+            <main className="blogLayout">
                 <Outlet/>
             </main>
-            <div>Example Layout</div>
         </div>
     );
 }
