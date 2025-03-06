b5620c3ad11b67c2b224ce2b31123d9229a9af1b
Jokoson <exampleexampleexample12135example@example.com>
2025-03-06 18:02:43 +0100

    Refactor AccountPage component to remove user context and simplify rendering logic

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,6 @@ import "./app.css";
 import MegaMenu from "@/components/ui/mega-menu";
 import {MegaMenuBreadcrumb} from "@/components/ui/mega-menu-breadcrumb";
 import LayoutContainer from "@/layout/layoutContainer";
-import {SupabaseAuthProvider} from "@/lib/supabaseAuthProvider";
 import {FooterContainer} from "@/layout/footerContainer";
 
 export function Layout({children}: { children: React.ReactNode }) {
@@ -42,9 +41,7 @@ export function Layout({children}: { children: React.ReactNode }) {
                 </LayoutContainer>
             </div>
         )}
-        <SupabaseAuthProvider>
-            {children}
-        </SupabaseAuthProvider>
+        {children}
         <FooterContainer/>
         <ScrollRestoration/>
         <Scripts/>
--- app/routes/_protected.account._index.tsx
+++ app/routes/_protected.account._index.tsx
@@ -1,7 +1,4 @@
-import {useContext} from "react";
-
 import type {Route} from "./+types/_protected.account._index";
-import {SupabaseAuthContext} from "@/lib/supabaseAuthProvider";
 import {LogoutButton} from "@/components/supabase/LogoutButton";
 import {Link} from "react-router";
 import {Button} from "@/components/ui/button";
@@ -25,23 +22,19 @@ export async function loader({params}: Route.LoaderArgs) {
 
 export default function AccountPage({loaderData}: Route.ComponentProps) {
     const {title} = loaderData
-    const {user} = useContext(SupabaseAuthContext);
     return (
-        user && (
-            <main className="flex items-center justify-center pt-16 pb-4">
-                <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-                    <b>Welcome {user?.email}!</b>
-                    {title}
-                    <div>
-                        <Link to={"/account/invoice"}>
-                            <Button>Invoice</Button>
-                        </Link>
-                    </div>
-                    <div>
-                        <LogoutButton/>
-                    </div>
+        <main className="flex items-center justify-center pt-16 pb-4">
+            <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+                {title}
+                <div>
+                    <Link to={"/account/invoice"}>
+                        <Button>Invoice</Button>
+                    </Link>
                 </div>
-            </main>
-        )
+                <div>
+                    <LogoutButton/>
+                </div>
+            </div>
+        </main>
     );
 }
