b57111711da6f907aa955dee3d35cb91fcab24b8
Jokoson <exampleexampleexample12135example@example.com>
2025-03-06 17:50:27 +0100

    Implement server-side login functionality and refactor LoginPage component

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -6,37 +6,48 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader({request}: any) {
-    const headers = new Headers()
+    const headers = new Headers();
 
-    const supabase = createServerClient(import.meta.env.VITE_SUPABASE_API_URL as string, import.meta.env.VITE_SUPABASE_ANON_KEY as string, {
-        cookies: {
-            getAll() {
-                return parseCookieHeader(request.headers.get('Cookie') ?? '')
+    // Supabase Client erstellen mit Cookie-Unterstützung
+    const supabase = createServerClient(
+        import.meta.env.VITE_SUPABASE_API_URL as string,
+        import.meta.env.VITE_SUPABASE_ANON_KEY as string,
+        {
+            cookies: {
+                getAll() {
+                    return parseCookieHeader(request.headers.get("Cookie") ?? "");
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({name, value, options}) =>
+                        headers.append("Set-Cookie", serializeCookieHeader(name, value, options))
+                    );
+                },
             },
-            setAll(cookiesToSet) {
-                cookiesToSet.forEach(({name, value, options}) =>
-                    headers.append('Set-Cookie', serializeCookieHeader(name, value, options))
-                )
-            },
-        },
-    })
+        }
+    );
+
+    // Benutzer mit E-Mail und Passwort anmelden
     const {data, error} = await supabase.auth.signInWithPassword({
-        email: 'demo@demo.demo',
-        password: 'demo',
-    })
+        email: "demo@demo.demo",
+        password: "demo",
+    });
 
-    console.log(data)
+    if (error) {
+        return new Response("Fehler bei der Anmeldung", {status: 401});
+    }
+    return new Response(JSON.stringify(data.user), {
+        headers,
+    });
 }
 
 export default function LoginPage({loaderData}: Route.ComponentProps) {
-
+    const data = loaderData;
+    console.log(JSON.parse(data));
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-5 min-h-0">
                 <h1>Sign in</h1>
-                <div>
-
-                </div>
+                <div></div>
             </div>
         </main>
     );
